414787a771252c873ff6edabd9f16f592b7a3a9f
vlaks <roletto.dylan@gmail.com>
2015-04-08 15:26:22 +0200

    creation de la classe Domaine Gestionnaire

---
+++ test/unit/toulousemusee/GestionnaireSpec.groovy
@@ -0,0 +1,40 @@
+package toulousemusee
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Gestionnaire)
+class GestionnaireSpec extends Specification {
+
+    @Unroll
+    void "test la validite d'un gestionnaire valide"(String unNom) {
+
+        given: "un gestionnaire initialise correctement"
+        Gestionnaire gestionnaire = new Gestionnaire(nom: unNom)
+
+        expect: "le gestionnaire est valide"
+        gestionnaire.validate() == true
+
+        where:
+        unNom = "Dupont"
+
+    }
+
+    @Unroll
+    void "test l'invalidite d'un gestionnaire non valide"(String unNom) {
+
+        given: "un gestionnaire initialise de maniere non valide"
+        Gestionnaire gestionnaire = new Gestionnaire(nom: unNom)
+
+        expect: "le gestionnaire est invalide"
+        gestionnaire.validate() == false
+
+        where:
+        unNom = ''
+
+    }
+}
